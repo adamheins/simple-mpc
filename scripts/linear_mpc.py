95af963ce78177851c5b5795c786055025645b01
Adam Heins <mail@adamheins.com>
2024-06-25 22:18:27 -0400

    Starting revisions.

--- scripts/linear_mpc.py
+++ scripts/linear_mpc.py
@@ -3,95 +3,94 @@
 # Simulation of comparison between PID and MPC for linear systems.
 
 import numpy as np
-import numpy.matlib as matlib
 import matplotlib.pyplot as plt
-import qpoases
+import cvxpy as cp
 
 
-class LinearSystem(object):
-    ''' Linear system of the form
-        x_{k+1} = A*x_k + B*u_k
-        y_k     = C*x_k '''
-    def __init__(self, x0, A, B, C):
+class LinearSystem:
+    """Linear system of the form
+    x_{k+1} = A*x_k + B*u_k
+    y_k     = C*x_k"""
+
+    def __init__(self, A, B):
         self.A = A
         self.B = B
-        self.C = C
 
-        self.u = 0
-        self.x = x0
+        self.nx = A.shape[0]  # state dimension
+        self.nu = B.shape[1]  # input dimension
 
-        self.n = A.shape[0]  # state dimension
-        self.m = B.shape[1]  # input dimension
-        self.p = C.shape[0]  # output dimension
+    def step(self, x, u):
+        u = np.minimum(u, 1.0)
+        u = np.maximum(u, -1.0)
+        return self.A @ x + self.B @ u
 
-    def apply(self, u):
-        if u < -1.0:
-            u = -1.0
-        elif u > 1.0:
-            u = 1.0
-        self.u = u
+    def lookahead(self, x0, xs_des, Q, R):
+        # TODO this is for single shooting
+        # TODO this can be cleaned up a lot! (almost everything can be
+        # pre-computed)
+        N = xs_des.shape[0]
 
-    def step(self):
-        self.x = self.A * self.x + self.B * self.u
-
-    def lookahead(self, x0, Yd, Q, R, N):
-        Abar = matlib.zeros((self.p*N, self.n))
-        Bbar = matlib.zeros((self.p*N, self.m*N))
-        Qbar = matlib.zeros((self.p*N, self.p*N))
-        Rbar = matlib.zeros((self.m*N, self.m*N))
+        Abar = np.zeros((self.nx * N, self.nx))
+        Bbar = np.zeros((self.nx * N, self.nu * N))
+        Qbar = np.zeros((self.nx * N, self.nx * N))
+        Rbar = np.zeros((self.nu * N, self.nu * N))
 
         # TODO right now we don't include a separate QN for terminal condition
         # Construct matrices
         for k in range(N):
-            l = k*self.p
-            u = (k+1)*self.p
+            l = k * self.nx
+            u = (k + 1) * self.nx
 
-            Abar[k*self.p:(k+1)*self.p, :] = self.C*self.A**(k+1)
-            Qbar[k*self.p:(k+1)*self.p, k*self.p:(k+1)*self.p] = Q
-            Rbar[k*self.m:(k+1)*self.m, k*self.m:(k+1)*self.m] = R
+            Abar[k * self.nx : (k + 1) * self.nx, :] = np.linalg.matrix_power(self.A, k + 1)
+            Qbar[k * self.nx : (k + 1) * self.nx, k * self.nx : (k + 1) * self.nx] = Q
+            Rbar[k * self.nu : (k + 1) * self.nu, k * self.nu : (k + 1) * self.nu] = R
 
             for j in range(k + 1):
-                Bbar[k*self.p:(k+1)*self.p, j*self.m:(j+1)*self.m] = self.C*self.A**(k-j-1)*self.B
+                Bbar[k * self.nx : (k + 1) * self.nx, j * self.nu : (j + 1) * self.nu] = (
+                    np.linalg.matrix_power(self.A, k - j - 1) @ self.B
+                )
 
-        # TODO note H is independt of x0 and Yd, and is thus constant at each
+        # TODO note H is independent of x0 and Yd, and is thus constant at each
         # step
-        H = Rbar + Bbar.T * Qbar * Bbar
-        g = (Abar * x0 - Yd).T * Qbar * Bbar
-
-        return np.array(H), np.array(g).flatten()
+        H = Rbar + Bbar.T @ Qbar @ Bbar
+        g = (Abar @ x0 - xs_des.flatten()).T @ Qbar @ Bbar
+        return H, g
 
 
 class PID(object):
-    ''' PID controller. '''
+    """PID controller."""
+
     def __init__(self, Kp, Ki, Kd):
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
 
-        self.e_prev = 0
-        self.e_int = 0
+        # previous error (for derivative term)
+        self.err_prev = 0
 
-    def control(self, e, dt):
-        ''' Generate control signal u to drive e to zero. '''
-        de = (e - self.e_prev) / dt
-        self.e_prev = e
-        self.e_int += dt * e
-        u = self.Kp * e + self.Kd * de + self.Ki * self.e_int
-        return u
+        # integral error
+        self.err_int = 0
+
+    def control(self, err, dt):
+        """Generate control signal u to drive e to zero."""
+        derr = (err - self.err_prev) / dt
+        self.err_prev = err
+        self.err_int += dt * err
+        u = self.Kp * err + self.Kd * derr + self.Ki * self.err_int
+        return np.atleast_1d(u)
 
 
 def step(t):
-    ''' Desired trajectory '''
+    """Desired trajectory"""
     # step
     if t < 1.0:
-        return 0.0
-    return 1.0
+        return [0.0, 0]
+    return [1.0, 0]
 
 
 def main():
-    n = 2  # state dimension
-    m = 1  # input dimension
-    p = 1  # output dimension
+    nx = 2  # state dimension
+    nu = 1  # input dimension
 
     N = 10  # number of lookahead steps
 
@@ -103,79 +102,63 @@ def main():
 
     # x+ = Ax + Bu
     # y  = Cx
-    A = np.matrix([[1, dt], [0, 1]])
-    B = np.matrix([[0], [dt]])
-    C = np.matrix([1, 0])
-    x0 = np.matrix([[0], [0]])
+    A = np.array([[1, dt], [0, 1]])
+    B = np.array([[0], [dt]])
 
-    sys1 = LinearSystem(x0, A, B, C)
-    sys2 = LinearSystem(x0, A, B, C)
+    sys = LinearSystem(A, B)
 
     # V = x'Qx + u'Ru
-    Q = matlib.eye(p) * 10
-    R = matlib.eye(p)
-    lb = np.ones(N) * -1.0
-    ub = np.ones(N) * 1.0
-    # nWSR = np.array([100])
-    nWSR = 100
+    Q = np.diag([10, 0])
+    R = np.eye(nu)
+    lb = np.ones(N * nu) * -1.0
+    ub = np.ones(N * nu) * 1.0
 
     t = np.zeros(num_steps)
-    x1 = np.zeros(sys1.n * num_steps)
-    x2 = np.zeros(sys2.n * num_steps)
-    yd = np.zeros(sys1.p * num_steps)
+    xs_des = np.zeros((num_steps, nx))
+    xs_pid = np.zeros((num_steps, nx))
+    xs_mpc = np.zeros((num_steps, nx))
 
-    qp = qpoases.PyQProblemB(sys2.m*N)
-    options = qpoases.PyOptions()
-    options.printLevel = qpoases.PyPrintLevel.NONE
-    qp.setOptions(options)
-
-    Yd = np.matrix([[step(t[0] + dt * j)] for j in range(N)])
-    H, g = sys2.lookahead(x0, Yd, Q, R, N)
-    qp.init(H, g, lb, ub, nWSR)
+    U = cp.Variable(nu * N)
 
     for i in range(num_steps - 1):
         # desired trajectory
-        yd[i] = step(t[i])
+        xs_des[i, :] = step(t[i])
 
         # PID control
-        x0 = np.matrix(x1[i*n:(i+1)*n]).T
-        e = yd[i] - sys1.C*x0
-        u = pid.control(e, dt)
+        err = xs_des[i, 0] - xs_pid[i, 0]
+        u = pid.control(err, dt)
+        xs_pid[i + 1, :] = sys.step(xs_pid[i, :], u)
 
-        sys1.apply(u)
-        sys1.step()
+        # MPC (single-shooting)
+        xs_des_horizon = np.array([step(t[i] + dt * j) for j in range(N)])
+        H, g = sys.lookahead(xs_mpc[i, :], xs_des_horizon, Q, R)
 
-        # MPC
-        x0 = np.matrix(x2[i*n:(i+1)*n]).T
-        Yd = np.matrix([[step(t[i] + dt * j)] for j in range(N)])
-        _, g = sys2.lookahead(x0, Yd, Q, R, N)
-        qp.hotstart(g, lb, ub, nWSR)
-        U = np.zeros(sys2.m * N)
-        qp.getPrimalSolution(U)
-        u = U[0]
+        objective = cp.Minimize(0.5 * cp.quad_form(U, H) + g @ U)
+        constraints = [U >= lb, U <= ub]
+        problem = cp.Problem(objective, constraints)
+        problem.solve()
+        u = U.value[:sys.nu]
 
-        sys2.apply(u)
-        sys2.step()
-
-        x1[(i+1)*sys1.n:(i+2)*sys1.n] = np.array(sys1.x).flatten()
-        x2[(i+1)*sys2.n:(i+2)*sys2.n] = np.array(sys2.x).flatten()
+        xs_mpc[i + 1, :] = sys.step(xs_mpc[i, :], u)
         t[i + 1] = t[i] + dt
 
     plt.subplot(211)
-    plt.plot(t, yd, label='desired')
-    plt.plot(t, x1[::2], label='actual')
-    plt.title('PID control')
+    plt.plot(t, xs_des[:, 0], label="desired")
+    plt.plot(t, xs_pid[:, 0], label="actual")
+    plt.title("PID control")
     plt.legend()
+    plt.grid()
 
     plt.subplot(212)
-    plt.plot(t, yd, label='desired')
-    plt.plot(t, x2[::2], label='actual')
-    plt.title('MPC')
+    plt.plot(t, xs_des[:, 0], label="desired")
+    plt.plot(t, xs_mpc[:, 0], label="actual")
+    plt.title("MPC (single shooting)")
     plt.legend()
-    plt.xlabel('Time (s)')
+    plt.grid()
+    plt.xlabel("Time (s)")
 
     plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
